0bc6f5dedede22c0e8ec97c44438a1662a585761
Alex <alexander.trebilcock@postgrad.student.ac.uk>
2022-03-01 14:33:37 +0000

    merged skeletons

--- question_classifier.py
+++ question_classifier.py
@@ -26,60 +26,6 @@ import numpy as np
 import configparser
 import argparse
 
-class BiLSTMTagger(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
-        
-        super(LSTMTagger, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
-        # The BiLSTM takes word embeddings as inputs, and outputs hidden states
-        # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, bidirectional=True)
-
-        # The linear layer that maps from hidden state space to tag space
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
-
-def configLoader(configFile):
-
-    # load config file (as found in coursework pdf)
-    config = configparser.ConfigParser()
-    config.sections()
-    config.read(configFile)
-
-    variables = []
-
-    for i in config.sections():
-        for j in config[i]:
-
-            globals()[j] = config[i][j] # make global variable with variable name in config file
-
-def train(configFile):
-
-    configLoader(configFile)
-
-def test(configFile):
-
-    configLoader(configFile)
-
-def main():
-    # check parsed arguements (as found in coursework pdf)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, required=True, help='Configuration file')
-    parser.add_argument('--train', action='store_true', help='Training mode - model is saved')
-    parser.add_argument('--test', action='store_true', help='Testing mode - needs a model to load')
-
-    args = parser.parse_args()
-    if args.train:
-        #call train function
-        train(args.config)
-    elif args.test:
-        #call test function
-        test(args.config)
-
-if __name__=="__main__":
-    main()
-    
-
 #Local Imports
 
 
@@ -107,9 +53,28 @@ def main(*args):
         
         #Preprocess data (stopwords, lemmatising)
         data = preprocessData(data) 
-          
+
+        # check parsed arguements (as found in coursework pdf)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', type=str, required=True, help='Configuration file')
+        parser.add_argument('--train', action='store_true', help='Training mode - model is saved')
+        parser.add_argument('--test', action='store_true', help='Testing mode - needs a model to load')
+
+        # file arguements
+        args = parser.parse_args()
+        
         for i in range(ensemble_count):
             
+            if args.train:
+            #call train function
+
+                train(args.config)
+
+            elif args.test:
+            #call test function
+            
+                test(args.config)
+
             #Train selected model (BOW or BiLSTM) if "train" arg specified
             if train_model:
                 trainModel()
@@ -128,9 +93,21 @@ def main(*args):
 
 def handleArguments():
     return true
-    
-    
-def readConfig():  
+                
+def readConfig(configFile): 
+
+    # load config file (as found in coursework pdf)
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read(configFile)
+
+    variables = []
+
+    for i in config.sections():
+        for j in config[i]:
+
+            globals()[j] = config[i][j] # make global variable with variable name in config file 
+
     if model_name not in model_sources:
         raise Exception("Error: unknown model specified: '" + model_name + "'.") 
     return true
