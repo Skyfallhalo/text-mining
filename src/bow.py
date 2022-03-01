731fc020b11476fdfc176de1bc22f8a0286f4c7d
Joshua Sayce <joshua.sayce@postgrad.manchester.ac.uk>
2022-03-01 15:18:52 +0000

    Moving to src folder, adding base code

---
+++ src/bow.py
@@ -0,0 +1,22 @@
+#//////////////////////////////////////////////////////////
+#   bow.py
+#   Created on:      01-Mar-2022 14:30:00
+#   Original Author: J. Sayce
+#   Specification:
+#
+#   base file for the 'bow' model, used by question_classifier.py.
+#
+#//////////////////////////////////////////////////////////
+
+def main(*args):
+    readConfig(args)
+    
+#Input: Config directory passed from question_classifier.py
+#Task: Populate config values by reading config.ini
+#Output: config.
+def readConfig(configFile): 
+    print("Debug")    
+    
+if __name__ == "__main__":
+    main(*sys.argv[1:])
+    sys.exit(0)
