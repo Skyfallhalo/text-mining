bc6aa7b3c5fa0bca30825c0a5265ae81e2a26f01
Joshua Sayce <joshua.sayce@postgrad.manchester.ac.uk>
2022-03-04 19:01:15 +0000

    Moved temporary code into own function.

--- src/ffnn_classifier.py
+++ src/ffnn_classifier.py
@@ -16,17 +16,17 @@ import numpy as np
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 
-
-
-from sklearn.linear_model import SGDClassifier
 from sklearn.datasets._base import Bunch
-from sklearn.datasets import fetch_20newsgroups
+from sklearn.linear_model import SGDClassifier
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 def main(*args):
     readConfig(args)
+    
+    
+def sklearnMethod():
     
     train_file, train_targets = getFromFile('../data/train.txt')
     test_file, test_targets = getFromFile('../data/test.txt')
@@ -63,12 +63,23 @@ def main(*args):
     predicted = clf.predict(new_data)
     
     #Display result of predictions
-    for doc, category in zip(docs_test, predicted):
-        print('%r \n %s' % (doc, test_data.target_names[category]))     
+    for doc, category, actual in zip(docs_test, predicted, test_data.target):
+        print('%r %s \n %s (%s) \n' % (doc, "Correct" if category == actual else "Incorrect", test_data.target_names[category], test_data.target_names[actual]))     
     
     #Display accuracy of predictions
-    model_accuracy = round(np.mean(predicted == test_data.target)*100, 2) 
-    print('Accuracy: {}%'.format(model_accuracy))   
+    score_accuracy = accuracy_score(test_data.target, predicted)
+    f1_accuracy = f1_score(test_data.target, predicted, average='micro')
+    print('Accuracy Score: {}%. F1 Accuracy: {}%.'.format(score_accuracy*100, f1_accuracy*100))   
+    
+    sample_question = ['What are shadows made of?']
+    X_new_counts = count_vect.transform(sample_question)
+    X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+    
+    predicted = clf.predict(X_new_tfidf)
+    
+    for doc, category in zip(sample_question, predicted):
+        print('%r => %s' % (doc, train_data.target_names[category]))    
+     
  
 def getFromFile(directory):
     data = []
@@ -89,6 +100,7 @@ def predictData(dataset, new_data_raw, new_data, clf):
     for doc, category in zip(new_data_raw, new_predicted):
         print('%r => %s' % (doc, dataset.target_names[category]))      
       
+    
     
 #Input: Config directory passed from question_classifier.py
 #Task: Populate config values by reading config.ini
