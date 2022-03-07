85586a1de1df167e56e1550bf7f2dcc414333aa8
Anchanted <931445831@qq.com>
2022-03-07 10:35:29 +0000

    Revert "Revert "Revert "BiLSTM demo model"""
    This reverts commit f55fb0eeed4a284a9bc66b7addda2d1a3331c8f9.

--- src/bilstm.py
+++ src/bilstm.py
@@ -19,34 +19,25 @@ import torch.optim as optim
 class BiLSTM(nn.Module):
     def __init__(self, embedding, hidden_dim, num_layers, class_num):
         super(BiLSTM, self).__init__()
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.hidden_size = hidden_dim
         self.num_layers = num_layers
         self.embedding = embedding
         embedding_dim = embedding.weight.size(dim=1)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True, bidirectional=True)
         self.fc = nn.Linear(hidden_dim * (2 if self.lstm.bidirectional else 1), class_num)  # 2 for bidirection
-        # self.fc = nn.Linear(hidden_dim, class_num)
 
     def forward(self, seq):
         # # set initial states
         # h0 = torch.zeros(self.num_layers * 2, seq.size(0), self.hidden_size).to(self.device)  # 2 for bidirection
         # c0 = torch.zeros(self.num_layers * 2, seq.size(0), self.hidden_size).to(self.device)
-        # print(seq.shape)
-        seq = self.embedding(seq).to(self.device)
-        # print(seq.shape)
+        seq = self.embedding(seq)
         # out, _ = self.lstm(seq, (h0, c0))
-        out, (ht, ct) = self.lstm(seq)
+        out, _ = self.lstm(seq)
         # print(out.shape)
-        # print(ht.shape)
-        # print(ct.shape)
-        # print(out[:, -1, :].shape)
-        # print((ht[0, :, :] + ht[1, :, :]).shape)
-        # out = self.fc(out[:, -1, :self.hidden_size] + out[:, 0, self.hidden_size:])
         out = self.fc(out[:, -1, :])
-        # print(out.shape)
-        # return F.softmax(out, dim=1)
-        return out
+        return F.softmax(out, dim=1)
+        # return out
 
 
 def main(embedding, config, class_num):
--- src/question_classifier.py
+++ src/question_classifier.py
@@ -1,4 +1,4 @@
-# //////////////////////////////////////////////////////////
+#//////////////////////////////////////////////////////////
 #   question_classifier.py
 #   the "wc" command
 #   Created on:      11-Oct-2021 12:55:00
@@ -15,352 +15,174 @@
 #   - Aggregates ensemble model results (if appropriate)
 #   - Handles formatting of feedback/errors from modules to user
 #
-# //////////////////////////////////////////////////////////
+#//////////////////////////////////////////////////////////
 
-# Library Imports
-import random
-import re
-import time
-from collections import Counter
-
+#Library Imports
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import random_split, DataLoader
-from sklearn.metrics import classification_report
 import numpy as np
 import configparser
 import argparse
-
-# Local Imports
 from bow import main as bow_main
 from bilstm import main as bilstm_main
 from embedding import main as embedding_main
 from ffnn_classifier import main as ffnn_main
-from question_dataset import QuestionDataset
 
-# Basic Structural Definitions
-model_sources = [['bow', bow_main], ['bilstm', bilstm_main]]
-model_dict = {'bow': bow_main, 'bilstm': bilstm_main}
+#Local Imports
 
-torch.manual_seed(1)
-random.seed(1)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
+#Basic Structual Definitions
+model_sources = [['bow',bow_main], ['bilstm',bilstm_main]]
 
 def main():
+    
+    #Read Arguments
+    args = handleArguments()
+    
+    #Read Config Files 
+    config = readConfig()
 
-    # Read Arguments
-    args = handle_arguments()
-
-    # Read Config Files
-    config = read_config(args.config)
-
-    # Retrieve
+    #Retrieve 
     if args.train:
-        data = load_data(config["Paths"]["path_train"])
 
-        stop_words = load_data(config["Paths"]["stop_words"])
+        dataDir = config["Paths"]["path_train"]
+        devDir = config["Paths"]["path_dev"]
+        
+        dev = loadData(devDir)
 
-        # Tokenize and gen. word embeddings (RandomInit, Pre-trained), if "train" arg specified
-        vocab_list = tokenise_data(data, stop_words)
-
-        # # Preprocess data (stopwords, lemmatising)
-        # vocab_list, vec_list = preprocess_data(vocab_list)
-
-        word_embedding, vocab_list = embedding_main(vocab_list, config)
-
-        encode_data, labels = encode_sentence(data, vocab_list, stop_words, int(config["Model"]["sentence_max_length"]))
-        classes = list(set(labels))
-        classes.sort()
-        dataset = QuestionDataset(encode_data, [classes.index(x) for x in labels])
-        train_ds, val_ds = random_split(dataset, [len(data) - int(len(data) * 0.1), int(len(data) * 0.1)])
-        batch_size = int(config["Network Structure"]["batch_size"])
-        train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-        val_dl = DataLoader(val_ds, batch_size=batch_size)
-
-        model_fn = model_dict[config["Model"]["model"]]
-        model = model_fn(word_embedding, read_config(config["Paths"]["%s_config" % (config["Model"]["model"])]), len(classes))
-        model.to(device)
-
-        model, optimiser = train_model(model, train_dl, val_dl,
-                                       epochs=int(config["Model Settings"]["epoch"]),
-                                       lr=float(config["Hyperparameters"]["lr_param"]),
-                                       opt_fn=torch.optim.SGD,
-                                       loss_fn=F.cross_entropy)
-
-        torch.save({
-            "model_state_dict": model.state_dict(),
-            # "optimizer_state_dict": optimiser.state_dict(),
-            "word_embedding_state_dict": word_embedding.state_dict(),
-            "stop_words": stop_words,
-            "vocab_list": vocab_list,
-            "classes": classes
-        }, config["Model"]["path_model"])
+        #Tokensive and gen. word embeddings (RandomInit, Pre-trained), if "train" arg specified
+        dev = tokeniseData(dev)        
+        
+        #Preprocess data (stopwords, lemmatising)
+        dev = preprocessData(dev) 
 
     elif args.test:
-        checkpoint = torch.load(config["Model"]["path_model"])
 
-        stop_words = checkpoint["stop_words"]
-        vocab_list = checkpoint["vocab_list"]
-        classes = checkpoint["classes"]
+        dataDir = config["Paths"]["path_train"]
 
-        word_embedding = nn.Embedding(checkpoint["word_embedding_state_dict"]["weight"].size(dim=0),
-                                      checkpoint["word_embedding_state_dict"]["weight"].size(dim=1))
-        word_embedding.load_state_dict(checkpoint["word_embedding_state_dict"])
+    data = loadData(dataDir)
 
-        model_fn = model_dict[config["Model"]["model"]]
-        model = model_fn(word_embedding, read_config(config["Paths"]["%s_config" % (config["Model"]["model"])]),
-                         len(classes))
-        model.load_state_dict(checkpoint['model_state_dict'])
-        model.to(device)
+    #Tokensive and gen. word embeddings (RandomInit, Pre-trained), if "train" arg specified
+    data = tokeniseData(data)        
+    
+    stopWords = loadData(config["Paths"]["stop_words"])
 
-        test_data = load_data(config["Paths"]["path_test"])
-        encode_data, labels = encode_sentence(test_data, vocab_list, stop_words, int(config["Model"]["sentence_max_length"]))
-        # test_ds = QuestionDataset(encode_data, [classes.index(x) for x in labels])
-        # test_dl = DataLoader(test_ds)
+    #Preprocess data (stopwords, lemmatising)
+    data = preprocessData(data, stopWords) 
 
-        test_model(model, torch.LongTensor(encode_data), torch.LongTensor([classes.index(x) for x in labels]), classes)
-
-    ensemble_size = int(config["Model"]["ensemble_size"])
+    ensemble_size = config["Model"]["ensemble_size"]
+    
+    word_embeddings = embedding_main(data)
 
     results = []
 
-    # for i in range(ensemble_size):
-    #
-    #     if args.train:
-    #         # Train selected model (BOW or BiLSTM) if "train" arg specified
-    #         results.append(train_model())
-    #
-    #     elif args.test:
-    #         # Test selected model (BOW or BiLSTM) if "test" arg specified
-    #
-    #         # test_model(model, test_dl, classes)
-    #
-    #         # Classify data (accuracy/F1 scores) produced by model if "test" arg specified
-    #         results.append(classify_model_output())
+    for i in range(ensemble_size):
+        
+        if args.train:
+            #Train selected model (BOW or BiLSTM) if "train" arg specified
 
-    # aggregate_results(results)
-    #
-    # display_results()
+            results.append(trainModel())
 
+        elif args.test:
+            #Test selected model (BOW or BiLSTM) if "test" arg specified
 
-def handle_arguments():
-    # check parsed arguments (as found in coursework pdf)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, required=True, help='Configuration file')
-    parser.add_argument('--train', action='store_true', help='Training mode - model is saved')
-    parser.add_argument('--test', action='store_true', help='Testing mode - needs a model to load')
+            testModel()
 
-    # file arguments
-    args = parser.parse_args()
-    return args
+            #Classify data (accuracy/F1 scores) produced by model if "test" arg specified
+            results.append(classifyModelOutput())
+            
+    aggregateResults(results)
+    
+    displayResults()
 
+def handleArguments():
 
-def read_config(config_file):
+     # check parsed arguements (as found in coursework pdf)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', type=str, required=True, help='Configuration file')
+        parser.add_argument('--train', action='store_true', help='Training mode - model is saved')
+        parser.add_argument('--test', action='store_true', help='Testing mode - needs a model to load')
+
+        # file arguements
+        args = parser.parse_args()
+        return args
+                
+def readConfig(configFile):
+
     # load config file (as found in coursework pdf)
     config = configparser.ConfigParser()
     config.sections()
-    config.read(config_file)
+    config.read(configFile)
 
     return config
 
+def writeConfig(configFile, data):
 
-def write_config(config_file, data):
     config = configparser.ConfigParser()
-
-    with open(config_file, 'w') as file:
+    
+    with open(configFile, 'w') as file:
         config.write(file)
 
 
-# Attempts to load data from the config-specified source for "Training Set 5".
-def load_data(directory):
-    data = []
-    with open(directory, "r") as f:  # get data
-        for line in f:
-            data.append(line.strip())
+#Attempts to load data from the config-specified source for "Training Set 5".
+def loadData(directory):
+
+    with open(directory, "r") as f: # get data
+        data = f.readlines()
 
     return data
 
 
-# Split the data into tokens.
-def tokenise_data(data, stop_words):
-    vocab_counter = Counter()
+#Split the data into tokens.
+def tokeniseData(data):
+
     for line in data:
-        [_, sentence] = line.split(" ", 1)
-        words = sentence.strip().lower().split(" ")
-        for word in words:
-            if re.match("[^a-z]*[a-z]+[^a-z]*", word) \
-                    and word not in stop_words:
-                vocab_counter.update([word])
-
-    vocab_list = []
-    for word, count in vocab_counter.items():
-        if count > 1:
-            vocab_list.append(word)
-
-    vocab_list.append("#unk#")
-
-    return vocab_list
+        line = line.split()
+    return data
 
 
-# Removes stopwords, lemma-izes, etc. according to config-specified rules.
-def preprocess_data(vocab_list):
-    trimmed_vocab_list = []
-    vec_list = []
-    vec_size = 0
-    with open("../data/glove.txt") as f:
-        for line in f:
-            [glove_word, vec_str] = line.split("\t", 1)
-            glove_word = glove_word.strip().lower()
-            idx = -1
-            for i in range(len(vocab_list)):
-                if glove_word == vocab_list[i]:
-                    idx = i
-                    break
-            # if idx != -1 or glove_word == "#unk#":
-            if idx != -1:
-                trimmed_vocab_list.append(glove_word)
-                vec = list(map(lambda x: float(x), vec_str.strip().split(" ")))
-                if len(vec) > vec_size:
-                    vec_size = len(vec)
-                vec_list.append(vec)
-                del vocab_list[idx]
-    trimmed_vocab_list.insert(0, "")
-    vec_list.insert(0, [0.0] * vec_size)
+#Removes stopwords, lemma-izes, etc. according to config-specified rules.
+def preprocessData(data, stopWords):
 
-    return trimmed_vocab_list, vec_list
+    for line in data:
+        line = line.replace('stopword', '')
+    return data
+    
+    
+#Uses either random, or pre-trained method to generate vector of word embeddings.
+def generateWordEmbeddings():
 
+    return [('lorem', 1), ('ipsum', 0)]
+   
+    
+#Calls external model (as specified by config) with data, recieves returned data, saves results.   
+def trainModel(data):
 
-# # Uses either random, or pre-trained method to generate vector of word embeddings.
-# def generate_word_embeddings():
-#
-#     return [('lorem', 1), ('ipsum', 0)]
-
-
-def encode_sentence(sentence_list, vocab_list, stop_words, max_len):
-    X = []
-    y = []
-    unk_idx = vocab_list.index("#unk#")
-    for sentence in sentence_list:
-        [label, sentence] = sentence.split(" ", 1)
-        words = sentence.strip().lower().split(" ")
-        seq = [0] * max_len
-        i = 0
-        for word in words:
-            if i == max_len:
-                break
-            # valid word
-            if re.match("[^a-z]*[a-z]+[^a-z]*", word):
-                # skip stop words
-                if word in stop_words:
-                    continue
-                try:
-                    seq[i] = vocab_list.index(word)
-                except ValueError:
-                    seq[i] = unk_idx
-                i += 1
-        X.append(seq)
-        y.append(label.lower().strip())
-    return X, y
-
-
-# Calls external model (as specified by config) with data, receives returned data, saves results.
-def train_model(model, train_dl, val_dl, epochs=10, lr=0.1, opt_fn=torch.optim.SGD, loss_fn=F.cross_entropy):
-    optimiser = opt_fn(filter(lambda e: e.requires_grad, model.parameters()), lr=lr)
-    for epoch in range(epochs):
-        start_time = time.time()
-
-        # Training phase
-        model.train()
-        train_total = 0
-        total_train_loss = 0.0
-        for x_batch, y_batch in train_dl:
-            # Forward pass and get prediction
-            x_batch = x_batch.type(torch.LongTensor).to(device)
-            y_batch = y_batch.to(device)
-            y_out = model(x_batch)
-
-            # Compute the loss, gradients, and update parameters
-            optimiser.zero_grad()
-            loss = loss_fn(y_out, y_batch)
-            loss.backward()
-            optimiser.step()
-
-            # Accumulate loss
-            train_total += y_batch.shape[0]
-            total_train_loss += loss.item() * y_batch.shape[0]
-        train_loss = total_train_loss / train_total
-
-        # Validation phase
-        model.eval()
-        correct = 0
-        val_total = 0
-        total_val_loss = 0.0
-        for x_batch, y_batch in val_dl:
-            # print(x_batch)
-            x_batch = x_batch.type(torch.LongTensor).to(device)
-            y_batch = y_batch.to(device)
-            y_out = model(x_batch)
-            loss = loss_fn(y_out, y_batch)
-            val_total += y_batch.shape[0]
-            total_val_loss += loss.item() * y_batch.shape[0]
-            # _, y_pred = torch.max(y_out, 1)
-            # print(y_out)
-            y_pred = torch.max(y_out, 1)[1]
-            correct += (y_pred == y_batch).sum().item()
-            # print(y_pred)
-            # print(y_batch)
-            # print((y_pred == y_batch), (y_pred == y_batch).sum())
-        val_loss = total_val_loss / val_total
-        val_acc = correct / val_total
-        # print(correct, val_total)
-
-        elapsed_time = time.time() - start_time
-
-        print('Epoch [{:02}/{:02}] \t loss={:.4f} \t val_loss={:.4f} \t val_acc={:.4f} \t time={:.2f}s'.format(
-            epoch + 1, epochs, train_loss, val_loss, val_acc, elapsed_time))
-
-    return model, optimiser
-
-
-# Attempts to run BOW or BiLSTM with data, receives returned data, and saves results.
-def test_model(model, x, y, classes):
-    x = x.to(device)
-    y = y.to(device)
-    y_out = model(x)
-    y_pred = torch.max(y_out, 1)[1]
-    # correct = (y_pred == y).sum().item()
-    # accuracy = correct / y_out.shape[0]
-    # print(y)
-    # print(y_pred)
-    y = y.cpu()
-    y_pred = y_pred.cpu()
-    idx_list = torch.ones(len(classes))
-    for n in y.unique():
-        idx_list[n.item()] = 0
-    for n in y_pred.unique():
-        idx_list[n.item()] = 0
-    target_names = classes.copy()
-    for i in torch.flip(torch.nonzero(idx_list, as_tuple=False), dims=[0]).squeeze():
-        del target_names[i]
-    print(classification_report(y.cpu(), y_pred.cpu(), target_names=target_names, zero_division=0))
-
-
-# Attempts to run FF-NN with data, receives returned data, and saves results.
-def classify_model_output():
+    for line in data:
+        line = line.split()
+    return data
+    
+#Attempts to run BOW or BiLSTM with data, recieves returned data, and saves results.    
+def testModel(data):
     print("Debug!")
-
-
-def aggregate_results():
+    
+    
+#Attempts to run FF-NN with data, recieves returned data, and saves results.
+def classifyModelOutput():
     print("Debug!")
-
-
-def display_results():
+    
+    
+def aggregateResults():
+    print("Debug!")
+   
+    
+def displayResults():
     print("Debug!")
 
 
 if __name__ == "__main__":
+    
     main()
+    exit()
