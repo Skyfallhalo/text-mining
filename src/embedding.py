558542571bc64d9ce11adcbd540cd92d19af9672
Zhaoyu Han <zhaoyu.han@postgrad.manchester.ac.uk>
2022-03-09 23:01:24 +0000

    Whole new method for embedding

--- src/embedding.py
+++ src/embedding.py
@@ -25,15 +25,32 @@
 
 import torch
 import torch.nn as nn
-#import sys
+import sys
 
-
-
-
-def main(config,corpus=None):
-    # readConfig(args)
+def main(vocab_list,config):
+    """
+    return embedding result and vocabulary.
+    
+    For pre0trained embeddings:
+     Config file:
+       pre_emb : true
+       path_pre_emb: a path to the embedding txt file
+       emb_freeze: true or false
+     Attributes:
+       vocab_list: a list of all words from the training data.
+    
+    For random initialized embeddings:
+      Config file:
+        pre_emb : false
+        word_embedding_dim: choose a value between 100 ~ 300
+      Attributes:
+        vocab_list: a list of words from training data.
+    
+    Returns:
+      embedding: a nn.embedding variable
+      trimmed_vocab_list: a trimmed final vocab list
+    """
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
     trimmed_vocab_list = []
     if config["Using pre-trained Embeddings"].getboolean("pre_emb"):
         vec_list = []
@@ -69,35 +86,79 @@ def main(config,corpus=None):
        # trimmed_vocab_list = [""] + vocab_list
        # embedding = nn.Embedding(len(trimmed_vocab_list), int(config["Network Structure"]["word_embedding_dim"]))
 
-        if corpus is not None:
-            vocabulary = {}
-            for sentence in corpus:
-                for token in sentence:
-                    if token not in vocabulary:
-                        vocabulary[token] = 1
-                    else:
-                        vocabulary[token] += 1
+#        if corpus is not None:
+#            vocabulary = {}
+#            for sentence in corpus:
+#                for token in sentence:
+#                    if token not in vocabulary:
+#                        vocabulary[token] = 1
+#                    else:
+#                        vocabulary[token] += 1
 
+#        trimmed_vocab_list = []
+#        for i in vocab_list:
+#            if min_count <= vocab_list[i]:
+#                trimmed_vocab_list.append(i)
 
-            trimmed_vocab_list = []
-            for i in vocabulary:
-                if min_count <= vocabulary[i]:
-                    trimmed_vocab_list.append(i)
+        vocab_list.insert(0,'')
+        trimmed_vocab_list = vocab_list
 
-            trimmed_vocab_list.append('UNK')
-            trimmed_vocab_list.insert(0,'')
+        vocabulary_size = len(trimmed_vocab_list)
+        embedding = nn.Embedding(vocabulary_size, int(config["Network Structure"]["word_embedding_dim"]))
 
-            vocabulary_size = len(trimmed_vocab_list)
-            embedding = nn.Embedding(vocabulary_size, int(config["Network Structure"]["word_embedding_dim"]))
-        
-        else:
-            sys.exit('No corpus to generate vocabulary and random embedding')
+#        else:
+#            sys.exit('No corpus to generate vocabulary and random embedding')
         
 
     embedding.to(device)
     embedding.weight.requires_grad = not config["Using pre-trained Embeddings"].getboolean("emb_freeze")
 
     return embedding, trimmed_vocab_list
+
+
+#def main(vocab_list, config):
+#    # readConfig(args)
+#    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#
+#    trimmed_vocab_list = []
+#    if config["Model"].getboolean("pre_emb"):
+#        vec_list = []
+#        vec_size = 0
+#        with open(config["Using pre-trained Embeddings"]["path_pre_emb"]) as f:
+#            for line in f:
+#                [glove_word, vec_str] = line.split("\t", 1)
+#                glove_word = glove_word.strip().lower()
+#                idx = -1
+#                for i in range(len(vocab_list)):
+#                    if glove_word == vocab_list[i]:
+#                        idx = i
+#                        break
+#                # if idx != -1 or glove_word == "#unk#":
+#                if idx != -1:
+#                    trimmed_vocab_list.append(glove_word)
+#                    vec = list(map(lambda x: float(x), vec_str.strip().split(" ")))
+#                    if len(vec) > vec_size:
+#                        vec_size = len(vec)
+#                    vec_list.append(vec)
+#                    del vocab_list[idx]
+#        trimmed_vocab_list.insert(0, "")
+#        vec_list.insert(0, [0.0] * vec_size)
+#
+#        # for e in vocab_list:
+#        #     print(e)
+#        # print(len(vocab_list))
+#
+#        weight = torch.FloatTensor(vec_list)
+#        # embedding = nn.Embedding.from_pretrained(weight, freeze=freeze)
+#        embedding = nn.Embedding.from_pretrained(weight)
+#    else:
+#        trimmed_vocab_list = [""] + vocab_list
+#        embedding = nn.Embedding(len(trimmed_vocab_list), int(config["Network Structure"]["word_embedding_dim"]))
+#
+#    embedding.to(device)
+#    embedding.weight.requires_grad = not config["Model"].getboolean("emb_freeze")
+#
+#    return embedding, trimmed_vocab_list
 
 
 
