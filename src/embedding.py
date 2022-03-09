e53f241ee4b71c071b848fa8bb1e2a7b6c743c1e
Zhaoyu Han <zhaoyu.han@postgrad.manchester.ac.uk>
2022-03-09 21:40:03 +0000

    introduce new way for loading pretained embeddings

--- src/embedding.py
+++ src/embedding.py
@@ -22,144 +22,180 @@
 #    main(*sys.argv[1:])
 #    sys.exit(0)
 
-import numpy as np
+
 import torch
 import torch.nn as nn
-
-def pretrained_embedding(vocab_fp, emb_fp):
-    """
-    return the pretrained embeddings
-    
-    Attributes:
-      vocab_fp: file path for vocabulary file (.txt)
-      emb_fp: file path for vector file (.txt)
-      
-    Returns:
-      vocab_list: a list that contains all words (including 'UNK' for words not in it)
-      emb: the nn.embedding type that contains embedding informations for all vocab 
-    """
-    
-    #load the vocab
-    with open(vocab_fp, "r") as my_file:
-        vocab_list = my_file.read().split(',')
-
-    vocab_list.remove('')
+#import sys
 
 
-    #load embedding vectors
-    with open(emb_fp, "r") as my_file:
-        content_list = my_file.read().split(';')
+
+def main(config,corpus=None):
+    # readConfig(args)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    trimmed_vocab_list = []
+    if config["Model"].getboolean("pre_emb"):
+        vec_list = []
+        vec_size = 0
+        with open(config["Using pre-trained Embeddings"]["path_pre_emb"]) as f:
+            for line in f:
+                [glove_word, vec_str] = line.split("\t", 1)
+                glove_word = glove_word.strip().lower()
+                idx = -1
+                for i in range(len(vocab_list)):
+                    if glove_word == vocab_list[i]:
+                        idx = i
+                        break
+                # if idx != -1 or glove_word == "#unk#":
+                if idx != -1:
+                    trimmed_vocab_list.append(glove_word)
+                    vec = list(map(lambda x: float(x), vec_str.strip().split(" ")))
+                    if len(vec) > vec_size:
+                        vec_size = len(vec)
+                    vec_list.append(vec)
+                    del vocab_list[idx]
+        trimmed_vocab_list.insert(0, "")
+        vec_list.insert(0, [0.0] * vec_size)
+
+        # for e in vocab_list:
+        #     print(e)
+        # print(len(vocab_list))
+
+        weight = torch.FloatTensor(vec_list)
+        # embedding = nn.Embedding.from_pretrained(weight, freeze=freeze)
+        embedding = nn.Embedding.from_pretrained(weight)
+    else:
+       # trimmed_vocab_list = [""] + vocab_list
+       # embedding = nn.Embedding(len(trimmed_vocab_list), int(config["Network Structure"]["word_embedding_dim"]))
+
+        if corpus is not None:
+            vocabulary = {}
+            for sentence in corpus:
+                for token in sentence:
+                    if token not in vocabulary:
+                        vocabulary[token] = 1
+                    else:
+                        vocabulary[token] += 1
 
 
-    vectors = []
-    for vector in content_list:
-        vec = []
-        elements = vector.split(' ')
-        for i in elements:
-            if '[' in i:
-                i = i.replace('[', '')
-            if ']' in i:
-                i = i.replace(']', '')
-            if '\n' in i:
-                i = i.replace('\n', '')
-            if i != '':
-                vec.append(float(i))
+            trimmed_vocab_list = []
+            for i in vocabulary:
+                if min_count <= vocabulary[i]:
+                    trimmed_vocab_list.append(i)
 
-        vectors.append(vec)
+            trimmed_vocab_list.append('UNK')
+            trimmed_vocab_list.insert(0,'')
 
-    vectors.remove([])  
-
-    vec = np.array(vectors)
-    weights = torch.FloatTensor(vec)
-    emb = nn.Embedding.from_pretrained(weights)
+            vocabulary_size = len(trimmed_vocab_list)
+            embedding = nn.Embedding(vocabulary_size, int(config["Network Structure"]["word_embedding_dim"]))
         
-    return vocab_list, emb
-    
-def random_embedding(corpus, min_count=3, vector_size=200):
-    """
-    return random initialized embeddings.
-    
-    Attributes:
-      corpus: list of lists, and each list contains words from each sentences.
-      min_count: the min count for words to appear in the vocabulary
-      vector_size: embedding dimensions
-      
-      
-    Returns:
-      real_vocab: a list that contains all words (including 'UNK' for words not in it)
-      emb: the nn.embedding type that contains embedding informations for all vocab 
-    """
-    vocabulary = {}
-    for sentence in corpus:
-        for token in sentence:
-            if token not in vocabulary:
-                vocabulary[token] = 1
-            else:
-                vocabulary[token] += 1
-
-
-    real_vocab = []
-    for i in vocabulary:
-        if min_count <= vocabulary[i]:
-            real_vocab.append(i)
-
-    real_vocab.append('UNK')
-    real_vocab.insert(0,'')
-
-    vocabulary_size = len(real_vocab)
-    emb = nn.Embedding(vocabulary_size, vector_size)
-
-    return real_vocab, emb
-
-
-
-def new_pretrained_embedding(vocab_fp, emb_fp,freeze=True):
-    """
-    return the pretrained embeddings
-    
-    Attributes:
-      vocab_fp: file path for vocabulary file (.txt)
-      emb_fp: file path for vector file (.txt)
-      freeze: bolean, whether to freeze the tensors.
-      
-    Returns:
-      vocab_list: a list that contains all words (including 'UNK' for words not in it)
-      emb: the nn.embedding type that contains embedding informations for all vocab 
-    """
-        #load the vocab
-    with open(vocab_fp, "r") as my_file:
-        vocab_list = my_file.read().split(',')
-
-    while '' in vocab_list:
-        vocab_list.remove('')
-    vocab_list.insert(0,'')
-
-    #load embedding vectors
-    with open(emb_fp, "r") as my_file:
-        content_list = my_file.read().split(';')
-
-
-    vectors = []
-    for vector in content_list:
-        vec = []
-        elements = vector.split(' ')
-        for i in elements:
-            if '[' in i:
-                i = i.replace('[', '')
-            if ']' in i:
-                i = i.replace(']', '')
-            if '\n' in i:
-                i = i.replace('\n', '')
-            if i != '':
-                vec.append(float(i)) 
-        vectors.append(vec)
-
-    vectors.remove([])  
-    vecs = np.array(vectors)
-    a = np.zeros((1,200))
-    vecs2 = np.concatenate((a,vecs))
-
-    weights = torch.FloatTensor(vecs2)
-    emb = nn.Embedding.from_pretrained(weights,freeze=freeze)
+        else:
+            sys.exit('No corpus to generate vocabulary and random embedding')
         
-    return vocab_list, emb
+
+    embedding.to(device)
+    embedding.weight.requires_grad = not config["Model"].getboolean("emb_freeze")
+
+    return embedding, trimmed_vocab_list
+
+
+
+
+
+#def main(config,corpus=None):
+#    """
+#    For use this module:
+#    1.add import sys
+#    2.add path_pre_emb_vocab in config
+#    3.
+#    """
+#    # readConfig(args)
+#    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#
+#    vocab_list = []
+#    if config["Model"].getboolean("pre_emb"):
+#        with open(config["Using pre-trained Embeddings"]["path_pre_emb"]) as f:
+#            vocab_list = f.read().split(',')
+#
+#        while '' in vocab_list:
+#            vocab_list.remove('')
+#        vocab_list.insert(0,'')
+#
+#        #load embedding vectors
+#        with open(config["Using pre-trained Embeddings"]["path_pre_emb_vocab"]) as my_file:
+#            content_list = my_file.read().split(';')
+#
+#
+#        vectors = []
+#        for vector in content_list:
+#            vec = []
+#            elements = vector.split(' ')
+#            for i in elements:
+#                if '[' in i:
+#                    i = i.replace('[', '')
+#                if ']' in i:
+#                    i = i.replace(']', '')
+#                if '\n' in i:
+#                    i = i.replace('\n', '')
+#                if i != '':
+#                    vec.append(float(i))
+#            vectors.append(vec)
+#
+#        vectors.remove([])
+#        vecs = np.array(vectors)
+#        a = np.zeros((1,vecs.shape[1]))
+#        vecs2 = np.concatenate((a,vecs))
+#
+#        weights = torch.FloatTensor(vecs2)
+#        embedding = nn.Embedding.from_pretrained(weights,freeze=freeze)
+#
+#
+#
+#    else:
+#        if corpus is not None:
+#            vocabulary = {}
+#            for sentence in corpus:
+#                for token in sentence:
+#                    if token not in vocabulary:
+#                        vocabulary[token] = 1
+#                    else:
+#                        vocabulary[token] += 1
+#
+#
+#            vocab_list = []
+#            for i in vocabulary:
+#                if min_count <= vocabulary[i]:
+#                    vocab_list.append(i)
+#
+#            vocab_list.append('UNK')
+#            vocab_list.insert(0,'')
+#
+#            vocabulary_size = len(vocab_list)
+#            embedding = nn.Embedding(vocabulary_size, vector_size)
+#
+#        else:
+#            sys.exit('No corpus to generate vocabulary and random embedding')
+#
+#
+#    embedding.to(device)
+#    embedding.weight.requires_grad = not config["Model"].getboolean("emb_freeze")
+#
+#    return embedding, vocab_list
+
+
+# Input: Config directory passed from question_classifier.py
+# Task: Populate config values by reading config.ini
+# Output: config.
+def readConfig(configFile):
+    print("Debug")    
+
+
+if __name__ == "__main__":
+    # main(*sys.argv[1:])
+    # sys.exit(0)
+    main()
+
+
+
+
+
