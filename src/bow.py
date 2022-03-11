42b6afc9a17db040e4289ee5d25d1ee2ac364357
Alex <alexander.trebilcock@postgrad.student.ac.uk>
2022-03-11 13:34:47 +0000

    final bow

--- src/bow.py
+++ src/bow.py
@@ -15,40 +15,33 @@ from torch.autograd import Variable
 
 class BOWClassifier(nn.Module):
 
-    def __init__(self, embedding, hidden_dim, num_layers):
+    def __init__(self, embedding, num_labels):
 
         super(BOWClassifier, self).__init__()
-        
-        # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
         self.embedding = embedding
+        embedding_dim = embedding.weight.size(dim=1)
+        self.linear = nn.Linear(embedding_dim, num_labels)
 
-        self.fc_hidden = nn.Linear(hidden_dim,hidden_dim)
-        self.bn_hidden = nn.BatchNorm1d(hidden_dim)
-        self.dropout = torch.nn.Dropout(p=0.5)
+    def forward(self, x):
 
-        self.fc_output = nn.Linear(hidden_dim, 1)
-        
-        self.loss = nn.BCEWithLogitsLoss()
+        bow_vector= []
 
-    def forward(self, x,target):
+        for i in x: # find vec_bow(x)
 
-        bow_embedding = []
-        for i in range(len(x)):
-            lookup_tensor = Variable(torch.LongTensor(x[i])).cuda()
-            embed = self.embedding(lookup_tensor)
-            embed = embed.mean(dim=0)
-            bow_embedding.append(embed)
-        bow_embedding = torch.stack(bow_embedding)
-    
-        h = self.dropout(F.relu(self.bn_hidden(self.fc_hidden(bow_embedding))))
-        h = self.fc_output(h)
-    
-        return self.loss(h[:,0],target), h[:,0]
+            lookup_tensor = Variable(torch.LongTensor(i)).cuda()
+            vec = self.embedding(lookup_tensor)
+            vec = vec.mean(dim=0)
+            bow_vector.append(vec)
+
+        bow_vector = torch.stack(bow_vector)
+
+        return F.log_softmax(self.linear(bow_vector), dim=1)
 
 
-def main(embedding, config, embedding_size):
+def main(embedding, config, num_labels):
 
-    return BOWClassifier(embedding, int(config["Arguments"]["hidden_dim"]), int(config["Arguments"]["num_layers"]))
+    return BOWClassifier(embedding, num_labels)
     
 #Input: Config directory passed from question_classifier.py
 #Task: Populate config values by reading config.ini
